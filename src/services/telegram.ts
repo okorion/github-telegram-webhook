14719138c5f1879d7956570c78f154df847f2d96
okorion <ok.or.orion@gmail.com>
2025-04-02 20:09:24 +0900

    refactor: sendTelegramMessage null 값 처리 삭제

--- src/services/telegram.ts
+++ src/services/telegram.ts
@@ -3,11 +3,8 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export async function sendTelegramMessage(message: string | null) {
+export async function sendTelegramMessage(message: string) {
   const telegramUrl = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`;
-  const hasNotMatchedFormatter = message === null;
-
-  if (hasNotMatchedFormatter) return;
 
   await axios.post(telegramUrl, {
     chat_id: process.env.TELEGRAM_CHAT_ID,
